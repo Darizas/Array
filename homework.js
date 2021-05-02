d600320e494fb0792d8bb897489d19882b84efd1
Darius <darius.jasiulis@gmail.com>
2021-05-02 23:24:16 +0300

    upd

--- homework.js
+++ homework.js
@@ -19,11 +19,16 @@ document.getElementById("objektas").innerHTML = (arr[0].autorius+' '+arr[0].teks
 
 document.getElementById("ilgis").innerHTML = arr.length;
 
+let find1 = arr.find(function (item, index, array) {
+    return item.autorius.amzius < 50;
+});
+document.getElementById("find").innerHTML = find1;
+console.log(find1);
 
-
-
-
-
+let find2 = arr.filter(function (item, index, array) {
+    return item.autorius.amzius < 50;
+});
+document.getElementById("filter").innerHTML = find2.length;
+console.log(find2);
 
 console.log(arr);
-
